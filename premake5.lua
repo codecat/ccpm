ebe841ffb5ae84332e424d02f6172052131ec681
Melissa Geels <spansjh@gmail.com>
2020-02-29 23:38:59 +0100

    Added SFML linking to example

--- premake5.lua
+++ premake5.lua
@@ -3,5 +3,6 @@ dofile('ccpm/ccpm.lua')
 ccpm_workspace 'test'
 	ccpm_consoleapp 'test'
 		ccpm_sources 'test'
+		ccpm_pkg 'sfml-all'
 
 ccpm_report()
--- test/foo.cpp
+++ test/foo.cpp
@@ -2,7 +2,26 @@
 
 #include <foo.h>
 
+#include <SFML/Graphics.hpp>
+
 void DoFoo()
 {
-	printf("Do foo!\n");
+	sf::RenderWindow window(sf::VideoMode(200, 200), "It works!");
+
+	sf::CircleShape shape(100.0f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+				break;
+			}
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
 }
