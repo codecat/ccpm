fb5334b41c3994581ca8c993b7b01e753c1c2551
Melissa Geels <spansjh@gmail.com>
2020-05-07 18:11:09 +0200

    Better pkgconfig support

--- ccpm/ccpm_pkg.lua
+++ ccpm/ccpm_pkg.lua
@@ -1,5 +1,6 @@
 local _pkg_find_paths = {
 	'/usr/lib/pkgconfig',
+	'/usr/local/lib/pkgconfig',
 	'/usr/lib/x86_64-linux-gnu/pkgconfig',
 	'/usr/share/pkgconfig',
 }
@@ -40,7 +41,7 @@ local function _pkg_open(name)
 		if line:len() > 0 and line:sub(1, 1) ~= '#' then
 			k, v = line:match('^([A-Z][^:]+): (.*)$')
 			if k ~= nil and v ~= nil then
-				properties[k] = _pkg_str_replace(v, variables)
+				properties[k:lower()] = _pkg_str_replace(v, variables)
 			else
 				k, v = line:match('^([^=]+)=(.*)$')
 				if k == nil or v == nil then
@@ -55,13 +56,16 @@ local function _pkg_open(name)
 	local pkg = {
 		id = name,
 
-		name = properties['Name'],
-		version = properties['Version'],
+		name = properties['name'],
+		version = properties['version'],
 
 		libs = {},
 		libdirs = {},
+		linkoptions = {},
+
 		includedirs = {},
 		defines = {},
+		buildoptions = {},
 
 		requires = {},
 	}
@@ -69,41 +73,72 @@ local function _pkg_open(name)
 	table.insert(CCPM_GLOBAL_PACKAGES, pkg)
 
 	--TODO: Libs.private (for static configurations)
-	-- Handle linker flags (-L and -l)
-	local libs = properties['Libs']
+	-- Handle linker flags (-L, -l, ...)
+	local libs = properties['libs']
 	if libs ~= nil then
 		for flag in libs:gmatch('-[^ ]+') do
-			flag_lib = flag:match('^-l(.*)$')
-			if flag_lib ~= nil then
-				table.insert(pkg.libs, flag_lib)
+			local found = false
+
+			-- Handle library links (-l)
+			if not found then
+				flag_lib = flag:match('^-l(.*)$')
+				if flag_lib ~= nil then
+					table.insert(pkg.libs, flag_lib)
+					found = true
+				end
 			end
 
-			flag_libdir = flag:match('^-L(.*)$')
-			if flag_libdir ~= nil then
-				table.insert(pkg.libdirs, flag_libdir)
+			-- Handle library directories (-L)
+			if not found then
+				flag_libdir = flag:match('^-L(.*)$')
+				if flag_libdir ~= nil then
+					table.insert(pkg.libdirs, flag_libdir)
+					found = true
+				end
+			end
+
+			-- Handle other linker flags (eg. -pthread)
+			if not found then
+				table.insert(pkg.linkoptions, flag)
 			end
 		end
 	end
 
-	-- Handle compiler flags (-I and -D)
-	local cflags = properties['Cflags']
+	-- Handle compiler flags (-I, -D, ...)
+	local cflags = properties['cflags']
 	if cflags ~= nil then
+
 		for flag in cflags:gmatch('-[^ ]+') do
-			flag_include = flag:match('^-I(.*)$')
-			if flag_include ~= nil and flag_include ~= '/usr/include' and flag_include ~= '/usr/local/include' then
-				table.insert(pkg.includedirs, flag_include)
+			local found = false
+
+			-- Handle include directories (-I)
+			if not found then
+				flag_include = flag:match('^-I(.*)$')
+				if flag_include ~= nil and flag_include ~= '/usr/include' and flag_include ~= '/usr/local/include' then
+					table.insert(pkg.includedirs, flag_include)
+					found = true
+				end
 			end
 
-			flag_define = flag:match('^-D(.*)$')
-			if flag_define ~= nil then
-				table.insert(pkg.defines, flag_define)
+			-- Handle defines
+			if not found then
+				flag_define = flag:match('^-D(.*)$')
+				if flag_define ~= nil then
+					table.insert(pkg.defines, flag_define)
+					found = true
+				end
+			end
+
+			-- Handle other compiler flags (eg. -pthread)
+			if not found then
+				table.insert(pkg.buildoptions, flag)
 			end
 		end
 	end
 
 	--TODO: Requires.private (for static configurations)
 	-- Load any dependencies
-	local requires = properties['Requires']
+	local requires = properties['requires']
 	if requires ~= nil then
 		for req in requires:gmatch('[^, ]+') do
 			dep = _pkg_from_cache(req)
@@ -120,6 +155,7 @@ end
 local function _pkg_add(pkg, added)
 	added = added or {}
 
+	-- Linker flags
 	for _, libdir in ipairs(pkg.libdirs) do
 		libdirs(libdir)
 	end
@@ -128,6 +164,11 @@ local function _pkg_add(pkg, added)
 		links(lib)
 	end
 
+	for _, flag in ipairs(pkg.linkoptions) do
+		linkoptions(flag)
+	end
+
+	-- Compiler flags
 	for _, dir in ipairs(pkg.includedirs) do
 		includedirs(dir)
 	end
@@ -136,6 +177,11 @@ local function _pkg_add(pkg, added)
 		defines(define)
 	end
 
+	for _, flag in ipairs(pkg.buildoptions) do
+		buildoptions(flag)
+	end
+
+	-- Package dependencies
 	for _, dep in ipairs(pkg.requires) do
 		if not added[dep.id] then
 			added[dep.id] = true
